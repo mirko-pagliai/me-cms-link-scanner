bf6eb2e88dd6f68072f9d7a6943dccfaf57b58f1
mirko-pagliai <mirko.pagliai@gmail.com>
2018-03-13 11:18:55 +0100

    first commit

---
+++ config/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of me-cms-link-scanner.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/me-cms-link-scanner
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+//Sets the default me-cms-link-scanner name
+if (!defined('ME_CMS_LINK_SCANNER')) {
+    define('ME_CMS_LINK_SCANNER', 'MeCmsLinkScanner');
+}
