c38b1c46744758a7011713306888296cc144ff54
mirko-pagliai <mirko.pagliai@gmail.com>
2020-01-21 14:13:14 +0100

    fixed

--- src/Controller/Admin/LinkScannerController.php
+++ src/Controller/Admin/LinkScannerController.php
@@ -46,7 +46,7 @@ class LinkScannerController extends AppController
 
         $logs = collection((new Folder($target))->find())
             ->map(function ($filename) use ($target) {
-                $path = $target . DS . $filename;
+                $path = $target . $filename;
 
                 return new Entity(compact('filename') + [
                     'filetime' => Time::createFromTimestamp(filemtime($path)),
